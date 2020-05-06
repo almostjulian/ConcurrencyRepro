84fe33bd23ea94912bddd7156ba53fce46a9da44
darkonejr <julian@darklight.org>
2020-05-06 15:32:24 -0500

    Add project files.

---
+++ WebApplication1/Global.asax.cs
@@ -0,0 +1,44 @@
+﻿using Funq;
+using ServiceStack;
+using ServiceStack.DataAnnotations;
+using ServiceStack.Text;
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication1
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : base("Concurrency Test API", typeof(AppHost).Assembly)
+        {            
+        }
+
+        public override void Configure(Container container)
+        {
+            Env.StrictMode = true;
+            JsConfig.Init(new ServiceStack.Text.Config
+            {
+                DateHandler = DateHandler.ISO8601,
+                SkipDateTimeConversion = true,
+                TextCase = TextCase.CamelCase,
+                ExcludeTypeInfo = true
+            });
+
+            SetConfig(new HostConfig
+            {
+                MapExceptionToStatusCode = { { typeof(KeyNotFoundException), 404 } },
+                Return204NoContentForEmptyResponse = true
+            }
+            );
+        }
+    }
+
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            new AppHost().Init();
+        }
+       
+    }
+}
---
+++ WebApplication1/ReproService.cs
@@ -0,0 +1,41 @@
+﻿using ServiceStack;
+
+
+namespace WebApplication1
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Initials { get; set; }
+        public string DisplayName { get; set; }
+        public string UserName { get; set; }
+        public string EmployeeId { get; set; }
+        public string EmployeeType { get; set; }
+        public string FwGroup { get; set; }
+        public string Email { get; set; }
+    }
+
+    [Route("/users/{UserName}", "GET", Summary = "Lists a single user")]
+    public class GetWindowsUser : IReturn<User>
+    {
+        public string UserName { get; set; }
+    }
+    public class ReproService : Service
+    {
+        [CacheResponse(Duration = 43200, MaxAge = 21600)]
+        public User Any(GetWindowsUser request)
+        {
+            return GetWindowsUser(request.UserName);
+        }
+        public static User GetWindowsUser(string userName)
+        {
+            string json =
+                $"{{\"firstName\":\"{userName}\",\"lastName\":\"{userName}\",\"displayName\":\"{userName}\",\"userName\":\"{userName}\",\"employeeId\":\"9999\",\"employeeType\":\"E\",\"fwGroup\":\"Test\",\"email\":\"{userName}@foo.com\"}}";
+
+            return json.FromJson<User>();
+        }
+
+    }
+}
